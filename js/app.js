14f05b2e9f3e81855d960b6c2e92f7c7e6fa7366
Mayo <anshgupta3552@gmail.com>
2022-05-21 13:34:24 +0530

    js fixes

--- js/app.js
+++ js/app.js
@@ -1,30 +1,8 @@
 const { exec } = require('node:child_process');
-const splash = document.querySelector('.splash');
-
-document.addEventListener('DOMContentLoaded', (e) => {
-  setTimeout(() => {
-    splash.classList.add('display-none');
-  }, 2500);
-})
 
 let viewportHeight = window.innerHeight;
 let viewportWidth = window.innerWidth;
 
 console.log(viewportHeight, viewportWidth);
-
-document.getElementById("ad").addEventListener("click", warning);
-
-function warning() {
-  window.alert("Select a city first!");
-}
-
-document.getElementById("dropdown").addEventListener("click", run_py);
-
-function run_py() {
-  exec('python ./python/delhi.py');
-  exec('python ./python/kolkata.py');
-  exec('python ./python/mumbai.py');
-  exec('python ./python/chennai.py');
-}
 
 console.log("IP Project 12 E, UI/UX desgined by Ansh Gupta.")
--- js/dropdown.js
+++ js/dropdown.js
@@ -1,3 +1,18 @@
+document.getElementById("ad").addEventListener("click", warning);
+
+function warning() {
+  window.alert("Select a city first!");
+}
+
+document.getElementById("dropdown").addEventListener("click", run_py);
+
+function run_py() {
+  exec('python ./python/delhi.py');
+  exec('python ./python/kolkata.py');
+  exec('python ./python/mumbai.py');
+  exec('python ./python/chennai.py');
+}
+
 document.addEventListener("click", e => {
   const isDropdownButton = e.target.matches("[data-dropdown-button]")
   if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
