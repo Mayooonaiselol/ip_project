1112c079efd5b56777e149296b9790c2faf21010
Mayooonaiselol <97210788+Mayooonaiselol@users.noreply.github.com>
2022-07-15 13:48:36 +0530

    book.js fixed

--- js/book.js
+++ js/book.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const cform = document.getElementById('bookform');
   cform.addEventListener("submit", handleSubmit_book);
 
-  function handleSubmit_book() {
+  function handleSubmit_book(event) {
     event.preventDefault();
     window.location.replace("../src/thanks.html")
   }
